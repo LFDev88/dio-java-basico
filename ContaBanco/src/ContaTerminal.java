2e58aeccf8de7f60fb59b78465109fdb9680ee6b
LFDev88 <luisfelipeborges_@hotmail.com>
2025-03-24 16:41:16 -0300

    atualização

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO: Conhecer e importar a classe Scanner
+        // Conhecer e importar a classe Scanner
         //Exibir as mensagens para o usuário
         //Obter pela scanner os valores digitados no terminal
         //Exibir a mensagem da conta criada
